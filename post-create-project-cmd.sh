9519b819653bb99ae7bf0c0c3d3394824250a039
Tim Nelles <tim.nelles@denkwerk.com>
2022-12-12 17:43:23 +0100

    Added fix to support sed on both bsd as well on gnu systems

--- post-create-project-cmd.sh
+++ post-create-project-cmd.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
 # set project name
-sed -i '' -e "s/COMPOSE_PROJECT_NAME=symfony_dockerized/COMPOSE_PROJECT_NAME=$(basename $(pwd))/g" ./.env
+sed -i.bak -e "s/COMPOSE_PROJECT_NAME=symfony_dockerized/COMPOSE_PROJECT_NAME=$(basename $(pwd))/g" ./.env
 
 # generate secrets
 NEW_APP_SECRET=$(php -r "echo bin2hex(openssl_random_pseudo_bytes(20));")
 NEW_DB_PASSWORD=$(php -r "echo bin2hex(openssl_random_pseudo_bytes(5));")
 
 # set secrets
-sed -i '' -e "s/your_db_password/$NEW_DB_PASSWORD/g" ./.env
-sed -i '' -e "s/your_db_password/$NEW_DB_PASSWORD/g" ./application/.env
-sed -i '' -e "s/your_app_secret/$NEW_APP_SECRET/g" ./application/.env
+sed -i.bak -e "s/your_db_password/$NEW_DB_PASSWORD/g" ./.env
+sed -i.bak -e "s/your_db_password/$NEW_DB_PASSWORD/g" ./application/.env
+sed -i.bak -e "s/your_app_secret/$NEW_APP_SECRET/g" ./application/.env
+
+rm -rf ./.env.bak
+rm -rf ./application/.env.bak
 
 # create .env.local
 cp ./application/.env ./application/.env.local
-sed -i '' -e "s/APP_ENV=prod/APP_ENV=dev/g" ./application/.env.local
-sed -i '' -e "s/APP_DEBUG=false/APP_DEBUG=true/g" ./application/.env.local
+sed -i.bak -e "s/APP_ENV=prod/APP_ENV=dev/g" ./application/.env.local
+sed -i.bak -e "s/APP_DEBUG=false/APP_DEBUG=true/g" ./application/.env.local
+
+rm -rf ./application/.env.local.bak
 
 # start & setup container
 docker-compose up -d
